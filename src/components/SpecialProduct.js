c39d5b90a3cc27aa6b30672a26f46ca25d1265b7
lion656544 <100365269+lion656544@users.noreply.github.com>
2024-01-29 03:51:36 +0530

    Completed Special Product page and the home page

--- src/components/SpecialProduct.js
+++ src/components/SpecialProduct.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import ReactStars from "react-rating-stars-component";
-
+// import { useTimer } from 'react-timer-hook';
+import ProgressBar from "@ramonak/react-progress-bar";
 
 import headphone from "../images/headphone.jpg"
+import headphone2 from "../images/headphone2.jpeg"
+
+import { Link } from 'react-router-dom';
 const SpecialProduct = () => {
   return (
     <div className='col-4'>
         <div className='special-product-card'>
             <div className='d-flex justify-content-between'>
-                <div className='special-img'>
-                    <img src={headphone} alt='special-product-1'/>
+                <div className='special-img '>
+                    <img src={headphone} className='img-fluid' alt='special-product-1'/>
+                    <div className='side-img d-flex '>
+                            <div className='img1 img-fluid'>
+                                <img src={headphone}  alt='special-product-1' />
+                            </div>
+                            <div className='img2 img-fluid'>
+                                <img src={headphone2}  alt='special-product-1' />
+                            </div>
+                        </div>
                 </div>
                 <div className='special-product-content'>
                     <h5 className='brand'>Boat</h5>
@@ -24,10 +36,32 @@ const SpecialProduct = () => {
                         activeColor="#ffd700"
                     />
                     <p className='price d-flex gap-10'>
-                        <div><span className='red '>$100.00</span></div>
+                        <div><span className='actual-price '>$100.00</span></div>
                         <div><strike>$410</strike></div>
                     </p>
+                    <div className='discount-till d-flex align-items-center gap-10'>
+                        <p className='mb-0'>
+                            <b>5</b> days
+                        </p>
+                        <span className='badge rounded-circle p-3 bg-danger'>24</span>:
+                        <span className='badge rounded-circle p-3 bg-danger'>11</span>:
+                        <span className='badge rounded-circle p-3 bg-danger'>50</span>
+                    </div>
+                    <div className='prod-count my-3'>
+                        <p>Products : 104</p>
+                        <div class="progress">
+                            <div className="progress-bar" 
+                                role="progressbar" 
+                                style={{width: '25%'}} 
+                                aria-valuenow="25" 
+                                aria-valuemin="0" 
+                                aria-valuemax="100">
+                            </div>
+                        </div>
+                    </div>
+                    <Link className='button mb-2'>Add to Cart</Link>
                 </div>
+                
             </div>
         </div>
     </div>
